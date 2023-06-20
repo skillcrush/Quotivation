6fce4cfcd3c769c19e38bdec3ccaf4f82242df2c
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-06-20 12:58:36 -0400

    Clean up CSS file and prepare for finishing touches

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Loader } from "react-feather";
-import CategoryForm from "./components/quotes/CategoryForm";
 import FavoriteQuotes from "./components/quotes/FavoriteQuotes";
 import Quotes from "./components/quotes/Quotes";
 import Message from "./components/Message";
@@ -14,7 +13,7 @@ function App() {
   const [category, setCategory] = useState("All");
   const [showMessage, setShowMessage] = useState(false);
   const [messageText, setMessageText] = useState("");
-  const [favoriteQuotes, setFavoriteQuotes] = useState(JSON.parse(window.localStorage.getItem("favoriteQuotes")) || []);
+  const [favoriteQuotes, setFavoriteQuotes] = useState([]);
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
   const categories = ["All", "Leadership", "Empathy", "Motivation", "Learning", "Success", "Empowerment"];
@@ -36,10 +35,6 @@ function App() {
   useEffect(() => {
     fetchQuotes();
   }, []);
-
-  useEffect(() => {
-    window.localStorage.setItem("favoriteQuotes", JSON.stringify(favoriteQuotes));
-  }, [favoriteQuotes]);
 
   const filteredQuotes = category !== "All" ? quotes.filter((quote) => quote.categories.includes(category)) : quotes;
 
