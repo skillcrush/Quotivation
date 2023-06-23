829ca7df96de7c89abb75e322b180b6de978b4df
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-06-23 14:05:30 -0400

    Simplify message component

--- src/components/quotes/QuoteCard.js
+++ src/components/quotes/QuoteCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Heart } from "react-feather";
 
 function QuoteCard({ quote, addToFavorites, favoriteQuotes }) {
-  const alreadyFavorite = favoriteQuotes && favoriteQuotes.find((favorite) => favorite.id === quote.id);
+  const alreadyFavorite = favoriteQuotes.find((favorite) => favorite.id === quote.id);
 
   const faveStyle = alreadyFavorite ? "#333" : "";
 
