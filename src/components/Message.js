d809f12a6833b4a8cec78b5aa34120498d462d3d
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-06-23 15:05:10 -0400

    Disappearing messages

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,6 +1,15 @@
 import React, { useEffect } from "react";
 
 const Message = ({ messageText, removeMessage }) => {
+  useEffect(() => {
+    const messageTimeout = setTimeout(() => {
+      removeMessage();
+    }, 1500);
+    window.setTimeout(messageTimeout);
+
+    return () => window.clearTimeout(messageTimeout);
+  });
+
   return (
     <div className='message'>
       <p>{messageText}</p>
