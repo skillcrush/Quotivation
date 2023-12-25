9a6fc0afb3278f0d395fe7063d521d3e6272a80f
lsavoie <twosavoie@gmail.com>
2023-12-25 00:06:43 -0600

    Updated Message.js file to match finished project solution code and code-along video

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,13 +2,13 @@ import React, { useEffect } from "react";
 
 const Message = ({ messageText, removeMessage }) => {
   useEffect(() => {
-    const messageTimeout = window.setTimeout(() => {
+    const messageTimeout = setTimeout(() => {
       removeMessage();
     }, 1500);
+    window.setTimeout(messageTimeout);
 
     return () => window.clearTimeout(messageTimeout);
-  },[]);
-
+  });
   return (
     <div className='message'>
       <p>{messageText}</p>
